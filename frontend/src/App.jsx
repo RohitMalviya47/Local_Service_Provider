b41bcafdac71ddeefbd3ea3bbafd58d2a70ec9d8
Rohit Malviya <127820049+RohitMalviya47@users.noreply.github.com>
2025-02-05 13:47:58 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Login from "./components/Login";
 import Footer from "./components/Footer";
+import Header from "./components/Header"
 import ServiceDetail from "./pages/ServiceDetail";
 import ForgotPassword from "./components/ForgetPassword";
-// import Onboarding from "./pages/Onboarding"; // ✅ Import Onboarding
+
 
 function App() {
   return (
@@ -19,7 +19,6 @@ function App() {
        {/* ✅ Default Page Onboarding */}
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
