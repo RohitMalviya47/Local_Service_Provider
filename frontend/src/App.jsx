cc3c3fd717dd3be5cbfd95fca519faaff92569d1
Rohit Malviya <127820049+RohitMalviya47@users.noreply.github.com>
2025-02-05 19:14:17 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header"
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Login from "./components/Login";
