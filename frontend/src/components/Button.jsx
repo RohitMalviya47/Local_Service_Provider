ef51c39465f836c8dcb7413f2e19a0b35204b6ac
Rohit Malviya <malviyarohit66155@gmail.com>
2025-02-07 11:07:33 +0530

    hiugpogdf

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -7,7 +7,9 @@ const Button = ({ children, onClick, className }) => {
       className={`px-6 py-2 bg-purple-600 text-white rounded-lg hover:bg-purple-700 ${className}`}
     >
       {children}
+      <button></button>
     </button>
+    
   );
 };
 
